e7df4788e7013141b5250402d31d0dbd82c081ee
Ismail Hussein <133203884+Ismail2hussein@users.noreply.github.com>
2023-05-19 21:35:32 -0700

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let A = 29
+let B = 19
+
+if (A > B) {
+    console.log("The largest number out of the two numbers is A");
+  } else {
+    console.log("Hi There !");
+  }
+  
